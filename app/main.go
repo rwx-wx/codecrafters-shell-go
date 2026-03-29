8c35d1ddcd04af98cbedbee13fb87d04ee58489d
rwx-wx <rwxrwx73@gmail.com>
2026-03-29 14:33:09 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -52,6 +52,7 @@ func main() {
 			dirName, err := os.Stat(args[0])
 			if err != nil {
 				fmt.Print("cd: ", args[0], ": No such file or directory\n")
+				continue
 			}
 
 			if dirName.IsDir() {
